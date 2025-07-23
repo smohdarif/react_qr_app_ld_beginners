5e1c4204fbf2f6989e6e93054b320546625336df
Arif Shaikh <smohdarif@gmail.com>
2025-07-23 13:57:50 -0400

    Update getClientKey.js

--- src/util/getClientKey.js
+++ src/util/getClientKey.js
@@ -1,13 +1,3 @@
-/*function getClientKey() {
-    let params = new URLSearchParams(document.location.search);
-    let clientKey = params.get('clientKey') || params.get('clientkey');
-    return clientKey;
-}
-
-export default getClientKey;*/
-
-
-
 function getClientKey() {
     let params = new URLSearchParams(document.location.search);
     let clientKey = params.get('clientKey') || params.get('clientkey');
